874a06358fd73f60d569c634b2cced99ee600be3
s4532350 <s4532350@SFNT118-21.ad.vu.edu.au>
2015-10-19 21:18:15 +1100

    Set up new Slim Framework. Creating a Form that calculates a users BMI

---
+++ app/src/routes.php
@@ -0,0 +1,44 @@
+<?php
+
+
+$app->get('/', function ($request, $response, $args) {
+    ddd($this->toc);
+    return $response->write("Hello from Getting slim with Slim");
+
+});
+
+
+$app->get('/docs', function ($request, $response, $args) {
+
+    $this->view->render($response, 'md_document.twig', [
+        'docs' => $this->toc,
+        'page_content' => "<h1>Get slim with Slim</h1>",
+    ]);
+});
+// this id's the route
+// using reg exp to validate,
+// w a html extension
+$app->get('/docs/{fname:[^\s]+.html}', function ($request, $response, $args) {
+
+    //see whats in a response and request
+    d($request); 
+    ddd($response);
+    
+    $dir = $this->application['docs_path'];
+    $docs = $this->toc; //take array
+    ddd($docs);
+    $idx = array_search($args['fname'], array_column($docs, 'slug')); //search array from index
+    $filename = $docs[$idx]['fname']; //work out filename
+
+    $full_filename = "{$dir}/{$filename}"; //take full filename
+    $parser = $this->md_parser; //taking out md_parser from container
+
+    $content = $parser->text(file_get_contents($full_filename)); //converting file full_content to html
+    $this->view->render($response, 'md_document.twig', [ 
+        'docs' => $docs,
+        'page_content' => $content,
+        'current_file' => $filename,
+    ]); //display file view render response, template response
+
+
+})->setName('docs');
